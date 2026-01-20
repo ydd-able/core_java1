1004208063597b7436731255c0af33adf4a532ae
ydd <1498396121@qq.com>
2026-01-21 01:44:03 +0800

    第一次提交

---
+++ javase/src/day01/Bank.java
@@ -0,0 +1,20 @@
+package day01;
+
+
+    public class Bank{
+        public static void main(String[] args){
+            System.out.println("======================");
+            System.out.println("               交通银行");
+            System.out.println("Bank of Communications");
+            System.out.println("======================");
+            System.out.println(        "欢迎光临交通银行");
+            System.out.println("您需要办理的业务是：现金汇款");
+            System.out.println("             您的排队号是：");
+            System.out.println("                 Q3019");
+            System.out.println("     您前面还有等待人数：11人");
+            System.out.println("2022-06-10    12:22:22");
+            System.out.println("温馨提示：办理业务需要携带");
+            System.out.println("         身份证等有效证件");
+        }
+    }
+
